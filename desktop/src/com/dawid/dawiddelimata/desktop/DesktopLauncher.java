5c96d2f2a2a0ebca5fa3992707d08ae621b828b6
Dawid Delimata <delimata.dawid@gmail.com>
2017-11-04 22:48:33 +0000

    init

---
+++ desktop/src/com/dawid/dawiddelimata/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.dawid.dawiddelimata.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.dawid.dawiddelimata.GettingUp;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GettingUp(), config);
+	}
+}
